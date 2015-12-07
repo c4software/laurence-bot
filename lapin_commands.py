75321e54bfcf07e1c70653237f0b501d44f4d613
Sébastien Malinge <valentinbrosseau@MacBook-Air-de-Valentin.local>
2015-12-07 14:36:00 +0100

    Ajout des slash

--- lapin_commands.py
+++ lapin_commands.py
@@ -3,7 +3,7 @@ from rest import callrest
 
 def launch_stop():
     try:
-        data = callrest(domain="vbr.dev", port=8000, ssl=False, path="stop", params={})[2]
+        data = callrest(domain="vbr.dev", port=8000, ssl=False, path="/stop", params={})[2]
         return "ok"
     except Exception as e:
         print (e)
@@ -12,7 +12,7 @@ def launch_stop():
 
 def launch_play(params):
     try:
-        data = callrest(domain="vbr.dev", port=8000, ssl=False, path="play", params=params)[2]
+        data = callrest(domain="vbr.dev", port=8000, ssl=False, path="/play", params=params)[2]
         return "ok"
     except Exception as e:
         print (e)
