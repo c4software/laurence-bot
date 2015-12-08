e35e4864e94a5d9abaa1f80db4831115e0551e83
Valentin Brosseau <c4software@gmail.com>
2015-12-08 17:51:33 +0100

    Passage avec la nouvelle présentation

--- commands/general.py
+++ commands/general.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 from .decorators import register_as_command
+from libs import make_message
+from settings import PSEUDO
 
 @register_as_command("aide")
 def cmd_aide(msg):
     from .decorators import commands
-    return "Mot clés disponibles : \r\n- {0}".format("\r\n- ".join(commands.keys()))
+    command_list = "\r\n- {0}".format("\r\n- ".join(commands.keys()))
+    return make_message(username=PSEUDO, icon_url="", fallback=command_list, pretext="", title="Liste des commandes :", title_link="", text=command_list, color="#7CD197")
 
 @register_as_command("bonjour")
 def cmd_bonjour(msg):
