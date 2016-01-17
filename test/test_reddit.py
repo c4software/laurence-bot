25cbd271ed74bf223cd6b31646e66bdd19ea2b94
Valentin Brosseau <c4software@gmail.com>
2016-01-17 22:52:02 +0100

    Modification test

--- test/test_reddit.py
+++ test/test_reddit.py
@@ -1,14 +1,9 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from commands.reddit import get_reddit_random
+from commands.reddit import get_reddit_random, cmd_random
 
 class TestReddit(unittest.TestCase):
-    def setUp(self):
-        print ("====================")
-        print ("Début du test Reddit")
-        print ("====================")
-
     def test_get_reddit_random(self):
         """ Test l'accès à Reddit """
         self.assertTrue(isinstance(get_reddit_random(), dict))
