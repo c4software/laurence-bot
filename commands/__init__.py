03a0b818708f66e9cb243cbb721340b54d53cab5
Antoine BERGER <valentinbrosseau@MacBook-Air-de-Valentin.local>
2016-02-15 09:14:47 +0100

    Mot clé mademoiselle

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import general, jenkins, lapin, reddit, chuck, joiesducode, vdm, fml, lesjoiesdusysadmin, madame, quizz, giphy
+from . import general, jenkins, lapin, reddit, chuck, joiesducode, vdm, fml, lesjoiesdusysadmin, madame, mademoiselle, quizz, giphy
