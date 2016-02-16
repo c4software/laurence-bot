b533f544d54b8b25a116daed025dda3537883f97
Valentin Brosseau <c4software@gmail.com>
2016-02-16 08:24:57 +0100

    Implémentation message quit

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import json
+import signal
+import sys
 
 from extended_BaseHTTPServer import serve,route, redirect, override
 from rest import callrest
@@ -31,12 +33,21 @@ def welcome():
     params = {"username": PSEUDO, "attachments": [{"color": "#3c901a", "title": PSEUDO, "text":"System Ready !".format(PSEUDO)}]}
     data = callrest(domain=MATTERMOST_DOMAIN, type="POST", path=MATTERMOST_PATH, params={"payload": json.dumps(params)})
 
+def aurevoir():
+    params = {"username": PSEUDO, "text": "Au revoir {0}".format("https://www.youtube.com/watch?v=uIMBjES4B4g")}
+    data = callrest(domain=MATTERMOST_DOMAIN, type="POST", path=MATTERMOST_PATH, params={"payload": json.dumps(params)})
+
 @route("/",["POST"])
 def form(**kwargs):
     kwargs['preview'] = False
     return chat(kwargs)
 
+def signal_handler(signal, frame):
+    aurevoir()
+    sys.exit(0)
+
 if __name__ == '__main__':
     print("Serving BOT on {0} port {1} ...".format(IP, PORT))
     #welcome()
+    signal.signal(signal.SIGINT, signal_handler)
     serve(ip=IP, port=PORT)
