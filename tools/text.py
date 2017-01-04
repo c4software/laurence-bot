942e77d81864b206ac84ea3927989f1097f84ac9
Valentin Brosseau <c4software@gmail.com>
2017-01-04 23:06:14 +0100

    Test

--- tools/text.py
+++ tools/text.py
@@ -16,8 +16,11 @@ def analyze_text(bot, update):
     text = demojize(update.message.text)
 
     # Analyse du texte en mode POS TAGGER
+    blob = tb(text)
+    text_keywords = [x[0] for x in blob.tags]
+
     if update.message.from_user.username in DEBUG_USER:
-        blob = tb(text)
+        update.message.reply_text ("Keywords: {0}".format(",".join(text_keywords)))
         update.message.reply_text (blob.tags)
         update.message.reply_text (blob.sentiment)
         #print (difflib.get_close_matches(args[0], commands))
