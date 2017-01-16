1fcfe203eec62600e667bed79f53f55b936aa160
Valentin Brosseau <c4software@gmail.com>
2017-01-16 22:03:58 +0100

    Ajout fonction pour traiter le texte pour extraire la commande

--- tools/libs.py
+++ tools/libs.py
@@ -10,8 +10,18 @@ def get_username(msg):
     return msg["user_name"][0]
 
 def get_debug_user_id(msg):
-    DEBUG_USER_ID[msg.username] = msg.id
+    if msg.id and msg.username in DEBUG_USER_ID:
+        DEBUG_USER_ID[msg.username] = msg.id
 
 def send_message_debug_user(bot, message=""):
     for user in DEBUG_USER_ID:
         bot.sendMessage(chat_id=DEBUG_USER_ID[user], text=message)
+
+def get_probable_command(text):
+    commande = text.split(' ')
+    commande = commande[0]
+
+    if commande.startswith("/"):
+        commande = commande[1:]
+
+    return commande
