91885ce1d69e401171cac2212068b5da9eee3a80
Valentin Brosseau <c4software@gmail.com>
2017-01-17 22:00:33 +0100

    Ajout d’un premier module « context », pour rendre le truc plus vivant

---
+++ commands/context.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from .decorators import register_as_command
+from commands.history import get_history
+from tools.libs import get_probable_command, get_username
+from commands.decorators import commands
+from settings import NA
+
+@register_as_command("plus", None, keywords=["encore"])
+def cmd_more(msg):
+    username = get_username(msg)
+    previous_text = get_history(username)[-2]
+
+    commande = get_probable_command(previous_text)
+    if commande in commands:
+        return commands[commande](msg)
+    else:
+        return None
