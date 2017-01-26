732056f97db6632009150ad0b1417546e179b51e
Valentin Brosseau <c4software@gmail.com>
2017-01-26 22:16:43 +0100

    Amélioration commande learn

--- commands/learn.py
+++ commands/learn.py
@@ -31,15 +31,44 @@ def write_learn():
 
 @register_as_command("learn", "", "Interne")
 def cmd_do_learn(msg):
+    """
+        Commande permettant de sauvegarder une correspondance Texte -> Commande.
+        :param msg["query"]: (Optionnel) Commande à associer au texte précédent.
+
+        ---------
+        Ex (Avec fourniture de commande) :
+        Vous : Laurence ?
+        Vous : /learn /echo Oui ?
+        Bot: Correspondance ajoutée « ('laurence', 'NNP') » -> « /echo Oui ? »
+        …
+        Vous : Laurence ?
+        Bot Oui ?
+
+        ---------
+        Ex (Avec fourniture de commande) :
+        Vous /echo Oui ?
+        Bot : Oui ?
+        Vous : Laurence ?
+        Vous : /learn
+        Bot: Correspondance ajoutée « ('laurence', 'NNP') » -> « /echo Oui ? »
+        …
+        Vous : Laurence ?
+        Bot Oui ?
+
+    """
     username    = get_username(msg)
     if not msg["query"]:
         command     = get_history(username)[-3]
     else:
         command = msg["query"]
     tags        = get_last_tags(username)
-    add_alias(command, tags)
+
+    if not tags:
+        return "Vous devez avoir précédement dit une phrase à ajouter"
 
     if is_telegram(msg) and is_debug(username):
+        # Ajout de l’alias
+        add_alias(command, tags)
         return emojize("Correspondance ajoutée « {0} » -> « {1} » :thumbs_up_sign:".format(tags, command))
     else:
         return emojize("Désolé le learn n’est pas disponible :unamused_face:")
