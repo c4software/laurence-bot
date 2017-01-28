de04d51a8dea0101b101061b57efa47cf183a58c
Valentin Brosseau <c4software@gmail.com>
2017-01-28 11:05:14 +0100

    Ajout du db_session pour obtenir la connexion a la bdd

---
+++ database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from settings import SQL_PARAMS
+
+engine = create_engine(SQL_PARAMS, convert_unicode=True, echo=False, pool_recycle=3600)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+def init_db():
+    import models.models
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
