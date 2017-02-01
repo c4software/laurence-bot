7b43ad8e0b36a42e77ce2ea2747c47a7ae48be41
Valentin Brosseau <c4software@gmail.com>
2017-02-01 21:33:14 +0100

    Ajout commande planning et ajout en db

--- commands/planification.py
+++ commands/planification.py
@@ -5,6 +5,8 @@ from commands.context import mark_for_awaiting_response
 from tools.libs import get_probable_command, username_or_channel
 from commands.decorators import commands
 
+from database import db_session
+from models.models import Task
 
 def extract_hours_minutes(query):
     '''
@@ -16,6 +18,18 @@ def extract_hours_minutes(query):
         return query[0], query[1]
     else:
         return None, None
+
+@register_as_command("planning", "Affiche la liste des commandes planifier", keywords=[])
+def cmd_planning(msg):
+    tasks = Task.query.filter_by(username=username_or_channel(msg)).all()
+    if tasks:
+        retour = "Voici la liste des commandes planifiés : \n"
+        for task in tasks:
+            retour = "{0} - « {1} » tous les jours à {2}\n".format(retour, task.commande, task.planned_time)
+    else:
+        retour = "Aucune commande planifiés."
+
+    return retour
 
 @register_as_command("planifier", "Planifier la réccurrence de la précédente commande", keywords=[])
 def cmd_planifier(msg):
@@ -34,12 +48,13 @@ def cmd_planifier(msg):
     - Lui : Trés bien … planification de « commande » à 10h
     '''
     previous_text = get_last_message(msg)
-    previous_text = get_probable_command(previous_text)
-    if previous_text and previous_text != "planifier":
+    if previous_text and "planifier" not in previous_text:
         heure, minute = extract_hours_minutes(msg["query"])
         if heure:
-            # TODO Planification
-            return "[TODO] Planification effective, vous allez recevoir automatiquement « {0} » tous les jours à {1}h{2}".format(previous_text, heure, minute)
+            task = Task(username_or_channel(msg), "{0}:{1}".format(heure, minute), previous_text)
+            db_session.add(task)
+            db_session.commit()
+            return "Parfait ! Vous allez recevoir automatiquement « {0} » tous les jours à {1}h{2}".format(previous_text, heure, minute)
         else:
             mark_for_awaiting_response(username_or_channel(msg), "planifier")
             return "À qu’elle heure voulez vous planifier l’envoi automatique de « {0} » ?".format(previous_text)
