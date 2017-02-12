67f7f3eb618b3b886bf16ce8628b0ab321baa52e
Valentin Brosseau <c4software@gmail.com>
2017-02-12 22:44:55 +0100

    Init hue

--- commands/hue.py
+++ commands/hue.py
@@ -6,3 +6,12 @@ from .decorators import register_as_command
 @register_as_command("hue", "Gestion des ampoules hue.", "Domotique")
 def cmd_hue(msg):
     return "Prochainement"
+
+def cmd_init_hue():
+    pass
+
+def cmd_set_level():
+    pass
+
+def cmd_change_room_state():
+    pass
