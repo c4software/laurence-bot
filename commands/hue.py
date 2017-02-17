5f0631e18c400e984017258ee557d5e466c99216
Valentin Brosseau <c4software@gmail.com>
2017-02-17 21:54:22 +0100

    Ajout base pour future module hue

--- commands/hue.py
+++ commands/hue.py
@@ -2,24 +2,32 @@
 
 from tools.rest import callrest
 from .decorators import register_as_command
+from settings import HUE_BRIDGE
 import logging
 
 try:
-    from qhue import Bridge
+    from qhue import Bridge, create_new_username
     @register_as_command("hue", "Gestion des ampoules hue.", "Domotique")
     def cmd_hue(msg):
         return "Prochainement"
 except:
     logging.debug ("qhue is required to use the hue module")
 
-def cmd_communicate_hue_server():
+def cmd_communicate_hue_server(ressource, bri):
+    username = "TODO"
+    b = Bridge(HUE_BRIDGE, username)
     pass
 
 def cmd_init_hue():
+    username = create_new_username(HUE_BRIDGE)
+    # TODO Save username
+
+def cmd_set_level(ressource, bri):
+    cmd_communicate_hue_server(ressource, bri)
     pass
 
-def cmd_set_level():
-    pass
-
-def cmd_change_room_state():
-    pass
+def cmd_change_room_state(ressource, state=True):
+    if state:
+        cmd_communicate_hue_server(ressource, 128)
+    else:
+        cmd_communicate_hue_server(ressource, 0)
