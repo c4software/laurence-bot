afa08026eac6933f6e4caf9b32e1be226886216b
Valentin Brosseau <c4software@gmail.com>
2017-03-09 21:55:36 +0100

    Type

--- commands/general.py
+++ commands/general.py
@@ -28,7 +28,7 @@ def cmd_echo(msg):
 @register_as_command("bonjour", "Heu… Bonjour?", keywords=["salut", "hey", "coucou"])
 def cmd_bonjour(msg):
     # Commande d’ambiance
-    return '{0} {1}, besoin d’/aide ?'.format(random.choice(["Salut", "Coucou", "Bonjour", "Hello", "Hoy"]),get_username(msg))
+    return '{0} {1}, besoin d’/aide ?'.format(random.choice(["Salut", "Coucou", "Bonjour", "Hello", "Hoy"]), get_username(msg))
 
 def get_command_list():
     '''
