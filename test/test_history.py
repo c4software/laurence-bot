87e5cd03866cc24183c4092614d9f7f3e2e6c511
Valentin Brosseau <c4software@gmail.com>
2017-03-12 21:51:59 +0100

    Ajout test pour la partie « historique »

---
+++ test/test_history.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from tools.libs import *
+from commands import *
+from commands.history import *
+
+class TestHistory(unittest.TestCase):
+    pseudo = "valentin_test"
+    def test_get(self):
+        data = make_attrs(self.pseudo, "", args=[""])
+        self.assertTrue(commands["historique"](data) is not None)
+
+    def test_get_last_message(self):
+        data = make_attrs(self.pseudo, "", args=[""])
+        self.assertTrue(get_last_message(data) is not None)
+
+    def test_get(self):
+        self.assertTrue(get_history(self.pseudo) is not None)
+
+    def test_insert(self):
+        add_history(self.pseudo, "Test")
+        self.assertTrue(get_history(self.pseudo) is not None)
+        remove_last_history(self.pseudo)
+
+    def test_get_last_tags(self):
+        save_last_tags(self.pseudo, "test")
+        self.assertTrue(get_last_tags(self.pseudo) == "test")
+
+
+if __name__ == '__main__':
+    unittest.main()
