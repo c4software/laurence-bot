fedb03d876be5ddedb3530b971d6d19c3dd11356
Valentin Brosseau <c4software@gmail.com>
2017-03-13 22:07:25 +0100

    Couverture uniquement sur la partie commande

--- run_coverage.sh
+++ run_coverage.sh
@@ -3,6 +3,6 @@
 # Couverture
 # pip install coverage coverage-badge
 
-coverage run --source=. -m unittest discover
+coverage run --source="commands,tools" -m unittest discover
 coverage report -m > test/coverage/coverage.txt
 coverage-badge -f -o test/coverage/coverage.svg
