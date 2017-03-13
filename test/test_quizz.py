a57febe5ef3447596a2b3b4b06e6deaeb7f155a5
Valentin Brosseau <c4software@gmail.com>
2017-03-13 23:14:20 +0100

    Ajout unittest dans la partie quizz

--- test/test_quizz.py
+++ test/test_quizz.py
@@ -10,9 +10,17 @@ class TestQuizz(unittest.TestCase):
     def test_get_question(self):
         self.assertIsInstance(get_question(), str)
 
+    def test_stop(self):
+        data = make_attrs(self.pseudo, "", args=[""])
+        self.assertTrue("Arret du quizz" in commands["stop"](data))
+
     def test_get_score(self):
         data = make_attrs(self.pseudo, "", args=[""])
         self.assertIsInstance(commands["score"](data), str)
+
+    def test_indice(self):
+        data = make_attrs(self.pseudo, "", args=[""])
+        self.assertIsInstance(commands["indice"](data), str)
 
 if __name__ == '__main__':
     unittest.main()
