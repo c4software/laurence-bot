e98032ab240ab7f814340535737b98908e683a7b
Valentin Brosseau <c4software@gmail.com>
2017-03-13 22:44:04 +0100

    Ajout planification

---
+++ test/test_planification.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from tools.libs import *
+from commands import *
+from commands.planification import extract_hours_minutes
+from commands.history import add_history
+
+class TestPlanification(unittest.TestCase):
+    pseudo = "test_valentin"
+    def test_extract_hours_minutes(self):
+        hours, minutes = extract_hours_minutes("08:00")
+        self.assertEqual(hours, "08")
+        self.assertEqual(minutes, "00")
+
+    def test_get_planning(self):
+        data = make_attrs(self.pseudo, "", args=[""])
+        self.assertIsInstance(commands["planning"](data), str)
+
+    def test_planifier_no_heure_no_prev_messages(self):
+        data = make_attrs(self.pseudo, "", args=[""])
+        self.assertTrue("Désolé" in commands["planifier"](data))
+
+if __name__ == '__main__':
+    unittest.main()
