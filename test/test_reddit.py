52e02c6e1c6d17f33d39b14bdb6a084109164405
Valentin Brosseau <c4software@gmail.com>
2017-03-16 22:10:15 +0100

    Ajout du test en random

--- test/test_reddit.py
+++ test/test_reddit.py
@@ -3,13 +3,14 @@ import unittest
 
 from tools.libs import *
 from commands import *
-from commands.giphy import get_gyphy
+from commands.reddit import get_reddit_random
 
 class TestReddit(unittest.TestCase):
     pseudo = "valentin_test"
     def test_cmd_gif(self):
         data = make_attrs(self.pseudo, "yolo", args=["yolo"])
         self.assertIsInstance(commands["random"](data), str)
+        self.assertTrue(get_reddit_random() is not None)
         self.assertIsInstance(commands["nsfw"](data), str)
         self.assertIsInstance(commands["image"](data), str)
         self.assertIsInstance(commands["cute"](data), str)
