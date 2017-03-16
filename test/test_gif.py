8bab1ced2a87ae70f623649283a90534beea03c9
Valentin Brosseau <c4software@gmail.com>
2017-03-16 22:23:36 +0100

    Ajout test as_message

--- test/test_gif.py
+++ test/test_gif.py
@@ -3,7 +3,7 @@ import unittest
 
 from tools.libs import *
 from commands import *
-from commands.giphy import get_gyphy
+from commands.giphy import get_gyphy, has_msg
 
 class TestGif(unittest.TestCase):
     def test_cmd_gif(self):
@@ -13,6 +13,12 @@ class TestGif(unittest.TestCase):
     def test_cmd_fail(self):
         data = make_attrs("valentin_test", "", args=[""])
         self.assertTrue("![image](" in commands["fail"](data))
+
+    def test_cmd_has_msg(self):
+        data = make_attrs("valentin_test", "", args=[""])
+        self.assertTrue(has_msg(data)[0] is False)
+        data = make_attrs("valentin_test", "Yolo", args=["yolo"])
+        self.assertTrue(has_msg(data)[0] is True)
 
     def test_get_gyphy(self):
         self.assertIsInstance(get_gyphy("yolo", False), str)
