1bb52e8a8fb41a47c230d86ca16ff6df08a3a90d
Valentin Brosseau <c4software@gmail.com>
2018-03-23 13:39:32 +0100

    Context et aide

--- commands/meeting.py
+++ commands/meeting.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+from .context import mark_for_awaiting_response
 from .decorators import register_as_command
+from tools.libs import get_username
 
-@register_as_command("meeting", "Stand", "Images")
+@register_as_command("meeting", "Enregistre une nouvelle entrée", "Meeting")
 def cmd_metting(msg):
+    mark_for_awaiting_response(get_username(msg), "meeting")
     return "Hey! T'as fait quoi hier ?"
