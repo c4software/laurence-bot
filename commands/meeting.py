0d587e3c4cb2cf90ce620d2961005e652be8b34a
Valentin Brosseau <c4software@gmail.com>
2018-03-24 22:29:59 +0100

    Base

--- commands/meeting.py
+++ commands/meeting.py
@@ -3,7 +3,20 @@ from commands.libs.context import mark_for_awaiting_response
 from commands.libs.decorators import register_as_command
 from tools.libs import get_username
 
+today_metting = []
+
 @register_as_command("meeting", "Enregistre une nouvelle entrée", "Meeting")
 def cmd_metting(msg):
-    mark_for_awaiting_response(get_username(msg), "meeting")
-    return "Hey! T'as fait quoi hier ?"
+    username = get_username(msg)
+
+    if username not in today_metting:
+        today_metting[username] = {}
+        
+    if "yesterday" not in today_metting[username]:
+        mark_for_awaiting_response(username, "meeting")
+        return "Hey! T'as fait quoi hier ?" 
+    elif "today" not in today_metting[username]:
+        mark_for_awaiting_response(username, "meeting")        
+        return "Et aujourd'hui tu prévois quoi ?"       
+    else:
+        return "C'est OK"
