d1818521d9b9e5c2ac1d9e90ef934805ee3957c6
Valentin Brosseau <c4software@gmail.com>
2018-03-27 17:18:27 +0200

    Fix reporting

--- commands/meeting.py
+++ commands/meeting.py
@@ -3,17 +3,16 @@ from commands.libs.context import mark_for_awaiting_response
 from commands.libs.decorators import register_as_command
 from tools.libs import get_username
 
-today_metting = []
+today_metting = {}
 
-@register_as_command("meeting_repport", "Affiche le rapport global", "Meeting")
+@register_as_command("meeting_report", "Affiche le rapport global", "Meeting")
 def cmd_report(msg):
     message = ""
-    for username in today_metting:
-        message = "@{0}: \r\n"
-        for event in today_metting[username]:
-            message += "\t{0} : \r\n\t\t {1}".format(event, today_metting[username][event])
-
-    today_metting = []
+    report = today_metting
+    for username in report:
+        message = "@{0}: \r\n".format(username)
+        for event in report[username]:
+            message += "\t\t{0} : \r\n\t\t {1}\r\n\r\n".format(event, report[username][event])
 
     return message
 
@@ -25,10 +24,11 @@ def cmd_metting(msg):
     if username not in today_metting:
         today_metting[username] = {}
 
-    if "yesterday" not in today_metting[username]:
-        today_metting[username]["yesterday"] = task
-    elif "today" not in today_metting[username]:
-        today_metting[username]["today"] = task
+    if task:
+        if "yesterday" not in today_metting[username]:
+            today_metting[username]["yesterday"] = task
+        elif "today" not in today_metting[username]:
+            today_metting[username]["today"] = task
 
 
     if "yesterday" not in today_metting[username]:
