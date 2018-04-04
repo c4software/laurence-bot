16c92a971d9b70b06bc42a219136b5b2fc635d9a
Valentin Brosseau <c4software@gmail.com>
2018-04-04 21:51:04 +0200

    Correction lint

--- database.py
+++ database.py
@@ -4,8 +4,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from settings import SQL_PARAMS
 
 engine = create_engine(SQL_PARAMS, convert_unicode=True, echo=False, pool_recycle=3600)
-db_session = scoped_session(sessionmaker(autocommit=False,
-                                         bind=engine))
+db_session = scoped_session(sessionmaker(autocommit=False, bind=engine))
+
 Base = declarative_base()
 Base.query = db_session.query_property()
 
