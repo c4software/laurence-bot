2028e53123a03e156c306038ffcedb5d705de268
Valentin Brosseau <c4software@gmail.com>
2018-04-14 10:10:02 +0200

    Lint

--- slack_start.py
+++ slack_start.py
@@ -12,21 +12,21 @@ from tools.libs import *
 from shared import save_data, clean_data
 from emoji import emojize, demojize
 
-import random, logging, os, sys, atexit, threading, time, re
+import logging, os, sys, atexit, threading, time, re
 
 # Set up basic logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(message)s', level=logging.INFO)
 
 MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
-slack_token = os.environ.get("LAURENCE_TOKEN_SLACK")
+SLACK_TOKEN = os.environ.get("LAURENCE_TOKEN_SLACK")
 
-if not slack_token:
+if not SLACK_TOKEN:
     logging.critical('Token absent (LAURENCE_TOKEN_SLACK="YOUR_TOKEN"')
     sys.exit()
 
-sc = SlackClient(slack_token)
-starterbot_id = None
-userslist = {}
+SLACKCLIENT = SlackClient(SLACK_TOKEN)
+SLACKBOT_ID = None
+USERLIST = {}
 
 def parse_bot_messages(slack_events):
     for event in slack_events:
@@ -39,7 +39,7 @@ def extract_command_query(commande):
     commande = commande.lower().split(' ')
     if commande[0].startswith("/"):
         commande[0] = commande[0][1:]
-        
+       
     return commande
 
 def parse_direct_mention(message_text):
@@ -51,38 +51,39 @@ def handle_command(text, channel, event):
     pseudo = get_slack_username(event["user"])
     attrs = analyze_text_slack(make_attrs(pseudo, text, commande[1:], event["channel"], None, {}))
 
-    # Extract data depuis la données analysé.
+    # Extract data depuis la données analysée.
     commande = extract_command_query(attrs["text"][0])
 
     if commande[0] in commands:
         retour = commands[commande[0]](attrs)
         if retour != "" and retour is not None:
-            if type(retour) is not str:
+            if not isinstance(retour, str):
                 retour = " ".join(retour)
-            post_message(retour)
+            post_message(retour, channel)
         else:
-            post_message("Désolé, je ne comprend pas encore votre demande… La liste des commandes est disponible via /aide")
+            post_message("Désolé, je ne comprend pas encore votre demande…", channel)
 
-def post_message(retour):
-    sc.api_call("chat.postMessage", link_names=1, channel=channel, text=retour)
+def post_message(retour, channel):
+    SLACKCLIENT.api_call("chat.postMessage", link_names=1, channel=channel, text=retour)
 
 def get_users_list_slack():
-    return {u["id"]:u["name"] for u in sc.api_call("users.list")["members"]}
+    return {u["id"]:u["name"] for u in SLACKCLIENT.api_call("users.list")["members"]}
 
-def get_slack_username(userId):
-    if userId in userslist:
-        return "@{}".format(userslist[userId])
+def get_slack_username(user_id):
+    global USERLIST
+    if id in USERLIST:
+        return "@{}".format(USERLIST[user_id])
     else:
-        ul = get_users_list_slack()
-        return ul[userId]
+        USERLIST = get_users_list_slack()
+        return get_slack_username(user_id)
 
 if __name__ == "__main__":
-    if sc.rtm_connect(with_team_state=False):
+    if SLACKCLIENT.rtm_connect(with_team_state=False):
         print("Laurence is ready !")
-        starterbot_id = sc.api_call("auth.test")["user_id"]
-        userslist = get_users_list_slack()
+        SLACKBOT_ID = SLACKCLIENT.api_call("auth.test")["user_id"]
+        USERLIST = get_users_list_slack()
         while True:
-            message, channel, event = parse_bot_messages(sc.rtm_read())
-            if message:
-                handle_command(message, channel, event)
+            MESSAGE, CHANNEL, EVENT = parse_bot_messages(SLACKCLIENT.rtm_read())
+            if MESSAGE:
+                handle_command(MESSAGE, CHANNEL, EVENT)
             time.sleep(0.5)
