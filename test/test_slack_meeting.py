80e13ed0c32967f677b2b54fcab6d8e7e60e6199
Valentin Brosseau <c4software@gmail.com>
2018-04-24 10:03:23 +0200

    Correction texte pour passer correctement les textes

--- test/test_slack_meeting.py
+++ test/test_slack_meeting.py
@@ -11,17 +11,17 @@ class TestMeeting(unittest.TestCase):
     pseudo = "@valentin"
 
     def test_meeting(self):
-        data = make_attrs(self.pseudo, "meeting", args={})
+        data = make_attrs(self.pseudo, "meeting", args=[""])
         self.assertEqual(commands["meeting"](data), MAP_TRADUCTION["yesterday"])
-        data = make_attrs(self.pseudo, "meeting", args="TEST 1")
+        data = make_attrs(self.pseudo, "meeting", args=["TEST 1"])
         self.assertEqual(commands["meeting"](data), MAP_TRADUCTION["today"])
-        data = make_attrs(self.pseudo, "meeting", args="TEST 2")
+        data = make_attrs(self.pseudo, "meeting", args=["TEST 2"])
         self.assertEqual(commands["meeting"](data), "Merci !")
 
         data = make_attrs(self.pseudo, "meeting_report", args="")
         report_value = commands["meeting_report"](data)
         self.assertIn("@valentin", report_value)
-        self.assertIn("T E S T   2", report_value)
+        self.assertIn("TEST 2", report_value)
 
 
 if __name__ == '__main__':
