b3bd85cad7b8d611a2bea96a69480ea7b99b28d5
Valentin Brosseau <c4software@gmail.com>
2018-06-10 23:25:26 +0200

    Init variables

--- slack_start.py
+++ slack_start.py
@@ -138,11 +138,12 @@ if __name__ == "__main__":
         SLACKBOT_ID = SLACKCLIENT.api_call("auth.test")["user_id"]
         USERLIST = get_users_list_slack()
         while True:
+            MESSAGE, CHANNEL, EVENT, MESSAGE_TYPE = None, None, None, None
             try:
                 MESSAGE, CHANNEL, EVENT, MESSAGE_TYPE = parse_bot_messages(SLACKCLIENT.rtm_read())
             except:
                 pass
-            
+
             if MESSAGE:
                 try:
                     handle_command(MESSAGE, CHANNEL, EVENT, MESSAGE_TYPE)
